048718e60bf813bb6d8ca98adb8ca736ac5c6e1a
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-25 15:40:24 -0600

    Add Distrito model and initial migrations for catalogo app; implement Cluster, Escuela, Monitor, and Director models with relationships in organizacion app

--- catalogo/models.py
+++ catalogo/models.py
@@ -70,3 +70,14 @@ class CatalogoRecomendacion(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Distrito(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Distrito"
+        verbose_name_plural = "Distritos"
+
--- organizacion/models.py
+++ organizacion/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalogo.models import Distrito
 
 # Create your models here.
+class Cluster(models.Model):
+    numero = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Cluster"
+        verbose_name_plural = "Clusters"
+
+    def __str__(self):
+        return self.numero
+
+class Monitor(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=8)
+
+    class Meta:
+        verbose_name = "Monitor"
+        verbose_name_plural = "Monitores"
+
+    def __str__(self):
+        return f"{self.usario.first_name} {self.usuario.last_name}"
+
+
+class Escuela(models.Model):
+    codigo = models.CharField(max_length=50, unique=True)
+    nombre = models.CharField(max_length=255, unique=True)
+    nombre_corto = models.CharField(max_length=100, unique=True)
+    distrito = models.ForeignKey(Distrito, on_delete=models.CASCADE)
+    estado = models.BooleanField(default=True)
+    monitor = models.ForeignKey(Monitor, on_delete=models.SET_NULL, null=True)
+
+
+    def __str__(self):
+        return f"{self.codigo} - {self.nombre} - {self.distrito.nombre}"
+    
+    class Meta:
+        verbose_name = "Escuela"
+        verbose_name_plural = "Escuelas"
+
+
+class Director(models.Model):
+    nombres = models.CharField(max_length=125)
+    apellidos = models.CharField(max_length=125)
+    telefono = models.CharField(max_length=9)
+    estado = models.BooleanField(default=True)
+    escuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Director"
+        verbose_name_plural = "Directores"
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos} - {self.escuela.nombre_corto}"
+
+
+
+
+
+
+
