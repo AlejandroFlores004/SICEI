1fae6438edec6f59778a18a5e17451b0f691d97f
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-25 14:01:03 -0600

    Add django_browser_reload for development URL configuration

--- sicei/urls.py
+++ sicei/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from sicei import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.urls import include, path
+    urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
