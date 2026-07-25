25c4de86a45bd9e5b9a1eac84cc3d3581cf787b5
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-25 14:14:55 -0600

    Add initial models and admin registration for catalogo app

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from catalogo.models import CatalogoAgenda, CatalogoFortaleza, CatalogoHallazgo, CatalogoLimitacion, CatalogoObjetivos, CatalogoParticipante
+
 # Register your models here.
+admin.site.register(CatalogoParticipante)
+admin.site.register(CatalogoObjetivos)
+admin.site.register(CatalogoAgenda)
+admin.site.register(CatalogoHallazgo)
+admin.site.register(CatalogoFortaleza)
+admin.site.register(CatalogoLimitacion)
--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
 # Create your models here.
+class CatalogoParticipante(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Participante"
+        verbose_name_plural = "Participantes"
+
+    def __str__(self):
+        return self.nombre
+
+class CatalogoObjetivos(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Objetivo"
+        verbose_name_plural = "Objetivos"
+
+    def __str__(self):
+        return self.nombre
+
+class CatalogoAgenda(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Agenda"
+        verbose_name_plural = "Agendas"
+
+    def __str__(self):
+        return self.nombre
+
+class CatalogoHallazgo(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Hallazgo"
+        verbose_name_plural = "Hallazgos"
+
+    def __str__(self):
+        return self.nombre
+
+class CatalogoFortaleza(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Fortaleza"
+        verbose_name_plural = "Fortalezas"
+
+    def __str__(self):
+        return self.nombre
+
+class CatalogoLimitacion(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Limitación"
+        verbose_name_plural = "Limitaciones"
+
+    def __str__(self):
+        return self.nombre
+
+class CatalogoRecomendacion(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Recomendación"
+        verbose_name_plural = "Recomendaciones"
+
+    def __str__(self):
+        return self.nombre
