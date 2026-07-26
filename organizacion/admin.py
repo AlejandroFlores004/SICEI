e77fc396243a4c726940ca2e0f585c38f9086fa0
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-25 22:28:04 -0600

    Register models in admin for organizacion app

--- organizacion/admin.py
+++ organizacion/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Cluster, Director, Monitor, Escuela
 # Register your models here.
+
+admin.site.register(Cluster)
+admin.site.register(Director)
+admin.site.register(Monitor)
+admin.site.register(Escuela)
