57040f0be69bbfc760b687990794bbbbdde81985
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-27 10:18:00 -0600

    Add models and admin registration for Informe, Acuerdo, Participante, Objetivo, Agenda, Hallazgo, Fortaleza, Limitacion, Recomendacion, and Otros

--- actividad/admin.py
+++ actividad/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Informe, Acuerdo, Participante, Objetivo, Agenda, Hallazgo, Fortaleza, Limitacion, Recomendacion, Otros
 
 # Register your models here.
+admin.site.register(Informe)
+admin.site.register(Acuerdo)
+admin.site.register(Participante)
+admin.site.register(Objetivo)
+admin.site.register(Agenda)
+admin.site.register(Hallazgo)
+admin.site.register(Fortaleza)
+admin.site.register(Limitacion)
+admin.site.register(Recomendacion)
+admin.site.register(Otros)
--- actividad/models.py
+++ actividad/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from organizacion.models import Escuela
+from catalogo.models import CatalogoParticipante, CatalogoAgenda, CatalogoFortaleza, CatalogoHallazgo, CatalogoLimitacion, CatalogoObjetivos, CatalogoRecomendacion
 
 # Create your models here.
+class Informe(models.Model):
+    actividad = models.TextField()
+    fecha = models.DateField()
+    esuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+
+class Acuerdo(models.Model):
+    nombre = models.CharField(max_length=250)
+    responsable = models.CharField(max_length=250)
+    fecha = models.DateField()
+    informe = models.ForeignKey(Informe, on_delete=models.CASCADE)
+
+class Participante(models.Model):
+    tipoParticipante = models.ForeignKey(CatalogoParticipante, on_delete=models.CASCADE)
+    cantidadHombre = models.IntegerField()
+    cantidadMujer = models.IntegerField()
+    informe = models.ForeignKey(Informe, on_delete=models.CASCADE)
+
+class Objetivo(models.Model):
+    objetivo = models.ForeignKey(CatalogoObjetivos, on_delete=models.CASCADE)
+    informe = models.ForeignKey(Informe, on_delete=models.CASCADE)
+
+class Agenda(models.Model):
+    agenda = models.ForeignKey(CatalogoAgenda, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    informe = models.ForeignKey(Informe, on_delete=models.CASCADE)
+
+class Hallazgo(models.Model):
+    hallazgo = models.ForeignKey(CatalogoHallazgo, on_delete=models.CASCADE)
+    informe = models.ForeignKey(Informe, on_delete=models.CASCADE)
+
+class Fortaleza(models.Model):
+    fortaleza = models.ForeignKey(CatalogoFortaleza, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    informe = models.ForeignKey(Informe, on_delete=models.CASCADE)
+
+class Limitacion(models.Model):
+    limitacion = models.ForeignKey(CatalogoLimitacion, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    informe = models.ForeignKey(Informe, on_delete=models.CASCADE)
+
+class Recomendacion(models.Model):
+    recomendacion = models.ForeignKey(CatalogoRecomendacion, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    informe = models.ForeignKey(Informe, on_delete=models.CASCADE)
+
+class Otros(models.Model):
+    descripcion = models.TextField()
+    informe = models.ForeignKey(Informe, on_delete=models.CASCADE)
