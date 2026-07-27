ef4dba63eb5480b73d08a8d49023d632a990dd38
AlejandroFlores004 <alejandroflores004@outlook.com>
2026-07-27 14:54:30 -0600

    Add dashboard and authentication templates; implement views and URL routing

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+def dashboardView(request):
+    return render(request,'dashboard.html')
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")  # change to your home url name
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")  # change to your home url name
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, "registration/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "Has cerrado sesión correctamente.")
+    return redirect("login")
